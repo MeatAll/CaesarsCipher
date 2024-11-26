030d0dde4201b1acd01dbd45e0d2dae897b7bf99
MeatAll <meatallist@gmail.com>
2024-11-26 06:39:09 +0300

    edit

--- lesson_16_1.py
+++ lesson_16_1.py
@@ -1,3 +1,5 @@
+import enchant
+
 class CaesarsCipher:
     """Класс для шифрования и дешифрования
     сообщений с использованием шифра Цезаря."""
@@ -48,6 +50,20 @@ class CaesarsCipher:
                 encrypted_message += char
         return encrypted_message
 
+    @staticmethod
+    def validate_decrypted_message(message):
+        d = enchant.Dict("en_US")
+        count = 0
+        words = message.split()
+        for word in words:
+            if not d.check(word):
+                pass
+            else:
+                count = count + 1
+        if count > 1:
+            return True
+
+
 
 if __name__ == '__main__':
     encrypted_password = "o3zR v..D0?yRA0R8FR8v47w0ER4.R1WdC!sLF5D"
@@ -56,7 +72,9 @@ if __name__ == '__main__':
     for key in range(len(cipher.coding_symbols)):
         decrypted_message = cipher.decrypt(encrypted_password, key)
         if ' ' in decrypted_message and decrypted_message.isascii():
-            print(f'{key}: {decrypted_message}')
+            if cipher.validate_decrypted_message(decrypted_message) is True:
+                print(f'{key}: {decrypted_message}')
+    print('Доп проверка:')
     key = 3
     decrypted_message = cipher.encrypt(decrypted_password, key)
     print(f'{key}: {decrypted_message}')
